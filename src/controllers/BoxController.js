6067de23155d9971495bc28e7410ccb95c275d8b
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2019-04-13 15:36:43 -0300

    Adicionado a funcionalidade de buscar o box já criado

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,6 +2,12 @@ const Box = require('../models/Box');
 
 class BoxController {
     async store(req, res) {
+        const boxName = req.body.title;
+
+        const boxFinded = await Box.find({ title: boxName });
+
+        if (boxFinded.length > 0) return res.json(boxFinded[0]);
+
         const box = await Box.create({ title: req.body.title });
 
         return res.json(box);
